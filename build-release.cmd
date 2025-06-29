879db84a7ee733a34b3fe7a867386f5a07e9ac55
kagw95 <aleksandr.kapustin.qubership@gmail.com>
2025-06-29 18:13:21 +0400

    Test database auth parameters are fixed.

--- build-release.cmd
+++ build-release.cmd
@@ -1,7 +1,7 @@
 del /s build-release-log.log
 CALL build-test.cmd
 CALL :CHECK_FAIL
-CALL mvn install -Dstage=release -DskipTests=true -Djdbc.Url=jdbc:postgresql://localhost:5432/datasets -Djdbc.User=postgres -Djdbc.Password=Qwe54321 > build-release-log.log
+CALL mvn install -Dstage=release -DskipTests=true -Djdbc.Url=jdbc:postgresql://localhost:5432/datasets -Djdbc.User=datasets -Djdbc.Password=datasets > build-release-log.log
 CALL :CHECK_FAIL
 GOTO :EOF
 :CHECK_FAIL
