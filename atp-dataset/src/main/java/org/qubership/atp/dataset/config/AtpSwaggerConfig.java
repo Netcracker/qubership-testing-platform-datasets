b5446fbdad872558956d6e5e66f98a12dcfcf939
kagw95 <aleksandr.kapustin.qubership@gmail.com>
2026-06-11 16:23:15 +0400

    fix(docs): Add License and Contact info to OpenAPI specifications and Swagger config.

--- atp-dataset/src/main/java/org/qubership/atp/dataset/config/AtpSwaggerConfig.java
+++ atp-dataset/src/main/java/org/qubership/atp/dataset/config/AtpSwaggerConfig.java
@@ -1,5 +1,5 @@
 /*
- * # Copyright 2024-2025 NetCracker Technology Corporation
+ * # Copyright 2024-2026 NetCracker Technology Corporation
  * #
  * # Licensed under the Apache License, Version 2.0 (the "License");
  * # you may not use this file except in compliance with the License.
@@ -57,7 +57,10 @@ public class AtpSwaggerConfig {
                         .addSecuritySchemes("Bearer Authentication", createApiKeyScheme()))
                 .info(new Info()
                         .title("ATP-Datasets")
-                        .license(new License().name("(C) Copyright Qubership Technology")));
+                        .license(new License()
+                                .name("Apache-2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")
+                        ));
     }
 
     /**
